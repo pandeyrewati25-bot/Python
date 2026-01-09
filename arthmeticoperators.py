cf40f740f3d6c4a60d262e92070987b5634808af
Rewati Pandey <pandeyrewati25@gmail.com>
2026-01-09 14:48:02 +0530

    Add files via upload

---
+++ arthmeticoperators.py
@@ -0,0 +1,37 @@
+a = 24
+b = 4
+c = 2
+d = 21
+
+#add
+print(a+b+c)
+
+#substract
+print(a-b-c)
+
+#multiply
+#single multiply
+print(a*b)
+
+#double multiply means power
+print(b**c)
+
+
+#division
+#single division = float value
+print(a/b)
+
+#double division = integer value
+print(a//b)
+
+#modulus means remainder
+print(a%b)
+print(d%c)
+
+####comparative operators
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(a!=b)
+print(a<=b)
